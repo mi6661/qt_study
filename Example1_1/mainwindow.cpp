acc67bb6911d2aff5c6bdad56cd36d9a3e15dce2
MI <#481447414@qq.com>
2025-05-19 22:48:32 +0800

    信号与槽学习完毕！

--- Example1_1/mainwindow.cpp
+++ Example1_1/mainwindow.cpp
@@ -1,15 +1,41 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //连接信号与槽 谁发出信号 发出什么信息号 谁处理信号 怎么处理信号
+    connect(ui->cmdLineEdit,&QLineEdit::returnPressed,this,&MainWindow::on_cmdbtn_clicked);
+    connect(ui->cancelbtn,&QPushButton::clicked,this,&MainWindow::on_cancelbtn_clicked);
+    connect(ui->browsebtn,&QPushButton::clicked,[this](){
+        QMessageBox::information(this,"信息","点击浏览");
+    });
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_cmdbtn_clicked()
+{
+    //获取lineEdit数据
+    QString program = ui->cmdLineEdit->text();
+
+    //输出提示
+    std::cout<<"cmdbtn was clicked!!"<<std::endl;
+    //获取启动程序的对象
+    QProcess *myProcess = new QProcess;
+    //启动进程
+    myProcess->start(program);
+}
+
+void MainWindow::on_cancelbtn_clicked(){
+    this->close();
+}
+
 
--- Example1_1/mainwindow.h
+++ Example1_1/mainwindow.h
@@ -2,18 +2,39 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QProcess>
+#include <QMessageBox>
 
+
+//QT的命名空间宏，用于防止名字冲突
 QT_BEGIN_NAMESPACE
+//建立一个Ui命名空间。命名空间中有下面定义的类。
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+//创建一个基础鱼QMainWindow的类
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    //构造函数
     MainWindow(QWidget *parent = nullptr);
+    //析构函数
     ~MainWindow();
+
+/*Qt的核心机制之一槽，用来响应信号。
+*
+*这处理响应信号是，如果connect使用旧版本方法，需要使用SLOT()来引入查函数
+*不仅有private slots，也有public、protected。
+*
+*如果使用的是旧版的SIGNAL(),SLOT()。则必须将槽函数写在这些地方。如果使用
+*C++11的新语法（函数指针方式），这不需要。
+*
+*/
+private slots:
+    void on_cmdbtn_clicked();
+    void on_cancelbtn_clicked();
 
 private:
     Ui::MainWindow *ui;
