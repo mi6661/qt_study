a6c1e4eca41e2cfc035a1d8152941081b71a0da2
MI <#481447414@qq.com>
2025-05-21 22:47:40 +0800

    objectTimer方式实现计时器完成！

--- objectTimer/widget.h
+++ objectTimer/widget.h
@@ -1,7 +1,10 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+
 #include <QWidget>
+#define TIMEOUT 1*1000
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -15,9 +18,21 @@ class Widget : public QWidget
 
 public:
     Widget(QWidget *parent = nullptr);
+    //计时器触发的回调函数。只要到达指定时刻，就会触发这个函数。
+    //这是一个虚函数。继承于QObject。
+    virtual void timerEvent(QTimerEvent *event);
     ~Widget();
+
+private slots:
+    //开始按钮和暂停按钮的槽函数
+    void on_btn_start_clicked();
+    void on_pushButton_2_clicked();
 
 private:
     Ui::Widget *ui;
+    //用于切换图片的一个计时器id。计时器的唯一标识符
+    int myTimerId;
+    //图片id。这个只是用于拼接图片路径。也可以说是图片的唯一标识符
+    int picId;
 };
 #endif // WIDGET_H
